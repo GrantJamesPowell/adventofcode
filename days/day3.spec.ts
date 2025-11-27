75236a27fc4283a508e6b4a2a75f8b4a416bea43
Grant Powell <gjamespowell@gmail.com>
2025-11-27 09:49:57 -0600

    Day3 attempt

--- days/day3.spec.ts
+++ days/day3.spec.ts
@@ -26,7 +26,6 @@ const runPt1 = (input: string) =>
 
 test("pt1", () => {
 	let testCase = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`;
-
 	expect(findMuls(testCase)).toEqual([
 		"mul(2,4)",
 		"mul(5,5)",
@@ -41,8 +40,88 @@ test("pt1", () => {
 		[8, 5],
 	]);
 
-  expect(runPt1(testCase)).toEqual(161);
+	expect(runPt1(testCase)).toEqual(161);
 
 	const answer = runPt1(data);
-  expect(answer).toEqual(185797128);
+	expect(answer).toEqual(185797128);
+});
+
+type Token = "do" | "don't" | ["mul", number, number];
+
+function* lex(input: string): IterableIterator<Token> {
+	let i = 0;
+
+	while (i < input.length) {
+		let rest = input.slice(i);
+
+		if (rest.startsWith("do()")) {
+			i += "do()".length;
+			yield "do";
+		} else if (rest.startsWith("don't()")) {
+			i += "don't()".length;
+			yield "don't";
+		} else if (rest.startsWith("mul(")) {
+			const match = rest.match(/mul\((\d+),\s*(\d+)\)/);
+
+			if (match === null) {
+				i++;
+				continue;
+			} else {
+				i += match[0].length;
+				let num1 = match[1]!;
+				let num2 = match[2]!;
+				yield ["mul", Number(num1), Number(num2)];
+			}
+		} else {
+			i++;
+		}
+	}
+}
+
+test("pt2", () => {
+	let testCase = `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`;
+	// do and dont lexing
+	expect([...lex("")]).toEqual([]);
+	expect([...lex("do")]).toEqual([]);
+	expect([...lex("do()")]).toEqual(["do"]);
+	expect([...lex("do()do()")]).toEqual(["do", "do"]);
+	expect([...lex("aaado()aaado()aaa")]).toEqual(["do", "do"]);
+	expect([...lex("aaadont()aaado()aaa")]).toEqual(["do"]);
+	expect([...lex("aaadon't()aaado()aaa")]).toEqual(["don't", "do"]);
+
+	// mul lexing
+	expect([...lex("mul(1,2)")]).toEqual([["mul", 1, 2]]);
+	expect([...lex("multiply(1,2)")]).toEqual([]);
+
+	expect([...lex(testCase)]).toEqual([
+		["mul", 2, 4],
+		"don't",
+		["mul", 5, 5],
+		["mul", 11, 8],
+		["mul", 11, 8],
+		"do",
+		["mul", 8, 5],
+	]);
+
+	let answer = (input: string) => {
+		let res = 0;
+		let enabled = true;
+
+		for (const token of lex(input)) {
+			if (token === "do") {
+				enabled = true;
+			} else if (token === "don't") {
+				enabled = false;
+			} else {
+				if (enabled) {
+					res += token[1] * token[2];
+				}
+			}
+		}
+
+		return res;
+	};
+
+	expect(answer(testCase)).toEqual(48);
+  expect(answer(data)).toEqual(93790718);
 });
