1acedc6b1883d6dbcc4a5e650f4054b57e09e687
Grant Powell <gjamespowell@gmail.com>
2025-12-01 01:35:00 -0500

    day 9 pt 2

--- 2021/days/day9.spec.ts
+++ 2021/days/day9.spec.ts
@@ -13,24 +13,63 @@ function* pointsOf(grid: Grid): Generator<Point & { val: number }> {
 	}
 }
 
-const adjPoints = ({ x, y }: Point): Point[] => [
+const adjPointsOf = ({ x, y }: Point) => [
 	{ x: x - 1, y },
 	{ x: x + 1, y },
 	{ x, y: y + 1 },
 	{ x, y: y - 1 },
 ];
 
-const p1 = (grid: Grid): number => {
-	const isLowPoint = (point: Point & { val: number }): boolean =>
-		adjPoints(point)
-			.map((adj) => grid[adj.y]?.[adj.x] ?? null)
-			.filter((adjValue) => adjValue !== null)
-			.every((adjValue) => adjValue > point.val);
+const lowPoints = (grid: Grid) =>
+	pointsOf(grid).filter((point) =>
+		adjPointsOf(point)
+			.map(({ x, y }) => grid[y]?.[x] ?? Infinity)
+			.every((adjValue) => adjValue > point.val),
+	);
 
-	return pointsOf(grid)
-		.filter(isLowPoint)
+const p1 = (grid: Grid): number =>
+	lowPoints(grid)
 		.map((point) => point.val + 1)
 		.reduce((acc, next) => acc + next, 0);
+
+const point2str = ({ x, y }: Point) => `${x}:${y}`;
+
+const p2 = (grid: Grid): number => {
+	const at = ({ x, y }: Point) => grid[y]?.[x] ?? null;
+
+	const seen: Set<string> = new Set();
+
+	const fill = (point: Point): number => {
+		if (seen.has(point2str(point))) {
+			return 0;
+		}
+
+		seen.add(point2str(point));
+
+		let size = 1;
+
+		for (const adjPoint of adjPointsOf(point)) {
+			const adjValue = at(adjPoint);
+
+			if (adjValue !== null && adjValue !== 9) {
+				size += fill(adjPoint);
+			}
+		}
+
+		return size;
+	};
+
+	let basinSizes: number[] = [];
+
+	for (const point of lowPoints(grid)) {
+		const size = fill(point);
+		basinSizes.push(size);
+	}
+
+	return basinSizes
+		.sort((a, b) => b - a)
+		.slice(0, 3)
+		.reduce((acc, next) => acc * next);
 };
 
 const parse = (data: string): Grid =>
@@ -51,4 +90,9 @@ const data = parse(readFileSync("./2021/inputs/day9.txt", "utf8").trim());
 test(p1.name, () => {
 	expect(p1(testData)).toEqual(15);
 	expect(p1(data)).toEqual(494);
+});
+
+test(p2.name, () => {
+	expect(p2(testData)).toEqual(1134);
+  expect(p2(data)).toEqual(1048128);
 });
