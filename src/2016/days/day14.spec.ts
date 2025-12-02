161a5dc0d8fa453c30f028c7ef68bd29a6263210
Grant Powell <gjamespowell@gmail.com>
2025-12-02 01:20:18 -0500

    Try to emit the hashes

--- src/2016/days/day14.spec.ts
+++ src/2016/days/day14.spec.ts
@@ -2,6 +2,14 @@ import { expect, test } from "bun:test";
 import { createHash } from "crypto";
 
 const md5 = (input: string) => createHash("md5").update(input).digest("hex");
+const hashAt = (salt: string, idx: number) => md5(`${salt}${idx}`);
+
+const analyzeHash = (hash: string) => {
+	const matches3 = /(.)\1\1/.exec(hash);
+	const matches5 = /(.)\1{4}/.exec(hash);
+
+	return { m3: matches3?.[1] ?? null, m5: matches5?.[1] ?? null };
+};
 
 function* hashStream(salt: string): Generator<string> {
 	let index = 0;
@@ -12,39 +20,56 @@ function* hashStream(salt: string): Generator<string> {
 	}
 }
 
-const p1 = (salt: string): number => {
+function* runP1(
+	salt: string,
+): Generator<{ hash: string; char: string; m3at: number; m5at: number }> {
 	const hashes = hashStream(salt).map((hash, idx) => [idx, hash] as const);
 
-	let found = 0;
 	let trips: Map<string, number[]> = new Map();
 
 	for (const [idx, hash] of hashes) {
-		const matches3 = /(.)\1\1/.exec(hash);
+		const { m3, m5 } = analyzeHash(hash);
 
-		if (matches3 !== null) {
-			const char = matches3[1]!;
-			if (!trips.has(char)) {
-				trips.set(char, []);
+		if (m3) {
+			if (!trips.has(m3)) {
+				trips.set(m3, []);
 			}
-			trips.get(char)!.push(idx);
+			trips.get(m3)!.push(idx);
 		}
 
-		const matches5 = /(.)\1{4}/.exec(hash);
+		if (m5) {
+			const tripsForChar = trips.get(m5) ?? [];
+			trips.set(m5, []);
 
-		if (matches5 !== null) {
-			const char = matches5[1]!;
-			const previous = trips.get(char) ?? [];
-		}
-
-		if (found === 64) {
-			return idx;
+			for (const m3at of tripsForChar) {
+				if (idx - m3at < 1000) {
+					yield { hash, char: m5, m3at, m5at: idx };
+				}
+			}
 		}
 	}
-};
+}
 
 const testData = "abc";
 const data = "qzyelonm";
 
-test(p1.name, () => {
-	expect(p1(testData)).toEqual(22728);
+test(hashAt.name, () => {
+	expect(hashAt(testData, 18)).toEqual(`0034e0923cc38887a57bd7b1d4f953df`);
+	expect(hashAt(testData, 39)).toEqual(`347dac6ee8eeea4652c7476d0f97bee5`);
+});
+
+test(analyzeHash.name, () => {
+	expect(analyzeHash(hashAt(testData, 18))).toEqual({ m3: "8", m5: null });
+	expect(analyzeHash(hashAt(testData, 39))).toEqual({ m3: "e", m5: null });
+	expect(analyzeHash(hashAt(testData, 816))).toEqual({ m3: "e", m5: "e" });
+});
+
+test(runP1.name, () => {
+	expect(
+		runP1(testData)
+			.map((x) => x.m3at)
+			.take(68)
+			.toArray()
+			.sort((a, b) => a - b),
+	).toEqual([]);
 });
